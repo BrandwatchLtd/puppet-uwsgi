5a0e5a522637b1445747aadf0abc045fd6a27cd5
Rob Wilson <roobert@gmail.com>
2016-05-20 14:35:47 +0100

    simplify install

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,21 +1,6 @@
 class uwsgi::install {
-  if ! defined(Package["python-dev"]) {
-    package { "python-dev":
-      ensure => present,
-    }
-  }
-
-  if ! defined(Package["python-pip"]) {
-    package { "python-pip":
-      ensure => present,
-    }
-  }
-
-  if ! defined(Package["uwsgi"]) {
-    package { "uwsgi":
-      ensure   => present,
-      provider => pip,
-      require  => [ Package["python-dev"], Package["python-pip"] ],
-    }
+  package { "uwsgi":
+    ensure   => present,
+    provider => 'apt',
   }
 }
